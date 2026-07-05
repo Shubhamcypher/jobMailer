7e5b6d07502a2d2d376bd26b7345be685700b6ac
Shubham <shubhamdevu023@gmail.com>
2026-07-05 21:25:37 +0530

    since we are using handlebars, so updating template service removing replace

--- server/services/templateService.js
+++ server/services/templateService.js
@@ -1,22 +1,21 @@
 import fs from "fs";
 import path from "path";
+import Handlebars from "handlebars";
+
+const templatePath = path.join(
+    process.cwd(),
+    "templates",
+    "coldEmail.hbs"
+);
+
+// Read only once when the server starts
+const source = fs.readFileSync(templatePath, "utf8");
+
+// Compile only once
+const template = Handlebars.compile(source);
 
 export const renderTemplate = (contact) => {
 
-    const templatePath = path.join(
-        process.cwd(),
-        "templates",
-        "coldEmail.html"
-    );
-
-    let html = fs.readFileSync(templatePath, "utf8");
-
-    html = html.replace(/{{name}}/g, contact.name);
-
-    html = html.replace(/{{company}}/g, contact.company);
-
-    html = html.replace(/{{title}}/g, contact.title);
-
-    return html;
+    return template(contact);
 
 };
