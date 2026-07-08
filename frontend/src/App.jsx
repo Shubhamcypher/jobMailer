9200d86f6e4f91d49fe6277ece5431da7dcbfda1
Shubham <shubhamdevu023@gmail.com>
2026-07-08 19:12:27 +0530

    added tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-     <h1>Hello</h1>
-    </>
-  )
+    return (
+        <div className="min-h-screen bg-slate-100">
+            <Dashboard />
+        </div>
+    );
 }
-
 export default App
